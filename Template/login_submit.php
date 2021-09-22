ca202dba38f8cb3b5ecd3f63eb644da75fcd1aca
lilmeowmeow25 <66581872+lilmeowmeow25@users.noreply.github.com>
2021-09-22 15:14:19 +0700

    Update login_submit.php

--- Template/login_submit.php
+++ Template/login_submit.php
@@ -2,6 +2,7 @@
     include 'config.php';
     include 'function.php';
 
+
     if (isset($_POST['username']) && $_POST['username'] != '' && 
         isset($_POST['password']) && $_POST['password'] != '' ) {
         $username = $_POST["username"];
@@ -15,5 +16,11 @@
             warning("Đăng nhập sai tài khoản hoặc mật khẩu");
         }
 
-    }
+    }if(isset($_POST['username']) && $_POST['username'] != ''){
+        warning("Vui lòng nhập mật khẩu");
+    }if(isset($_POST['password']) && $_POST['password'] != '') {
+        warning("Vui lòng nhập tài khoản");
+    }else{
+        warning("Vui lòng nhập tài khoản và mật khẩu");
+    } 
 ?>
